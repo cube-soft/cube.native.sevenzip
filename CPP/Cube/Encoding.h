4ffcebed92b3f7fb742f73d6556dc06fac839c5a
clown <tt.clown@gmail.com>
2017-08-18 16:16:40 +0900

    Add debugging log module.

--- CPP/Cube/Encoding.h
+++ CPP/Cube/Encoding.h
@@ -15,8 +15,7 @@
 /// limitations under the License.
 ///
 /* ------------------------------------------------------------------------- */
-#ifndef CUBE_ENCODING_H
-#define CUBE_ENCODING_H
+#pragma once
 
 #include <string>
 #include <winnls.h>
@@ -25,175 +24,189 @@
 
 namespace Cube {
 namespace Encoding {
-    namespace Details {
-        class Initializer {
-        public:
-            Initializer() { babel::init_babel(); }
-        };
+
+namespace Details {
+    class Initializer {
+    public:
+        Initializer() { babel::init_babel(); }
+    };
+}
+
+/* ------------------------------------------------------------------------- */
+///
+/// Kinds
+///
+/// <summary>
+/// 文字コードを表す列挙型です。
+/// </summary>
+///
+/* ------------------------------------------------------------------------- */
+enum Kinds {
+    Ascii           = babel::base_encoding::ansi,
+    Jis             = babel::base_encoding::jis,
+    ShiftJis        = babel::base_encoding::sjis,
+    EucJp           = babel::base_encoding::euc,
+    Utf8            = babel::base_encoding::utf8,
+    Unicode         = babel::base_encoding::utf16,
+    Unknown         = -1
+};
+
+/* ------------------------------------------------------------------------- */
+///
+/// Conversion
+///
+/// <summary>
+/// 文字コードを変換するためのクラスです。
+/// </summary>
+///
+/* ------------------------------------------------------------------------- */
+class Conversion {
+public:
+    typedef std::basic_string<char>    ascii_string;
+    typedef std::basic_string<wchar_t> unicode_string;
+
+    /* --------------------------------------------------------------------- */
+    ///
+    /// Initialize
+    ///
+    /// <summary>
+    /// 初期化を行います。
+    /// </summary>
+    ///
+    /// <remarks>
+    /// TODO: 最終的には Initialize 関数をなくす方法を検討する。
+    /// </remarks>
+    ///
+    /* --------------------------------------------------------------------- */
+    static void Initialize() {
+        static Details::Initializer initializer;
     }
 
     /* --------------------------------------------------------------------- */
     ///
-    /// Kinds
+    /// Guess
     ///
     /// <summary>
-    /// 文字コードを表す列挙型です。
+    /// 文字コードを推測します。
     /// </summary>
     ///
     /* --------------------------------------------------------------------- */
-    enum Kinds {
-        Ascii           = babel::base_encoding::ansi,
-        Jis             = babel::base_encoding::jis,
-        ShiftJis        = babel::base_encoding::sjis,
-        EucJp           = babel::base_encoding::euc,
-        Utf8            = babel::base_encoding::utf8,
-        Unicode         = babel::base_encoding::utf16,
-        Unknown         = -1
-    };
-
-    /* --------------------------------------------------------------------- */
-    ///
-    /// Conversion
-    ///
-    /// <summary>
-    /// 文字コードを変換するためのクラスです。
-    /// </summary>
-    ///
-    /* --------------------------------------------------------------------- */
-    class Conversion {
-    public:
-        typedef std::basic_string<char>    ascii_string;
-        typedef std::basic_string<wchar_t> unicode_string;
-
-        /* ----------------------------------------------------------------- */
-        ///
-        /// Initialize
-        ///
-        /// <summary>
-        /// 初期化を行います。
-        /// </summary>
-        ///
-        /// <remarks>
-        /// TODO: 最終的には Initialize 関数をなくす方法を検討する。
-        /// </remarks>
-        ///
-        /* ----------------------------------------------------------------- */
-        static void Initialize() {
-            static Details::Initializer initializer;
-        }
-
-        /* ----------------------------------------------------------------- */
-        ///
-        /// Guess
-        ///
-        /// <summary>
-        /// 文字コードを推測します。
-        /// </summary>
-        ///
-        /* ----------------------------------------------------------------- */
-        static int Guess(const ascii_string& src) {
-            auto result = babel::analyze_base_encoding(src).get_strict_result();
-            switch (result) {
-            case Ascii:
-            case ShiftJis:
-                return ShiftJis;
-            case EucJp:
-            case Jis:
-            case Utf8:
-            case Unicode:
-                return result;
-            default:
-                break;
-            }
+    static int Guess(const ascii_string& src) {
+        auto result = babel::analyze_base_encoding(src).get_strict_result();
+        switch (result) {
+        case babel::base_encoding::ansi:
+        case babel::base_encoding::sjis:
+        case babel::base_encoding::euc:
+        case babel::base_encoding::jis:
+        case babel::base_encoding::utf8:
+        case babel::base_encoding::utf16:
+            return result;
+        default:
             return Unknown;
         }
+    }
 
-        /* ----------------------------------------------------------------- */
-        ///
-        /// ToUnicode
-        ///
-        /// <summary>
-        /// Unicode に変換します。
-        /// </summary>
-        ///
-        /* ----------------------------------------------------------------- */
-        static unicode_string ToUnicode(const ascii_string& src, int encoding) {
-            switch(encoding) {
-            case ShiftJis:
-                return babel::sjis_to_unicode(src);
-            case EucJp:
-                return babel::euc_to_unicode(src);
-            case Jis:
-                return babel::jis_to_unicode(src);
-            case Utf8:
-            {
-                auto copy = src;
-                Mac::Normalize(copy);
-                return babel::utf8_to_unicode(copy);
-            }
-            case Unicode:
-            default:
-                break;
-            }
-            return Widen(src);
+    /* --------------------------------------------------------------------- */
+    ///
+    /// ToUnicode
+    ///
+    /// <summary>
+    /// Unicode に変換します。
+    /// </summary>
+    ///
+    /* --------------------------------------------------------------------- */
+    static unicode_string ToUnicode(const ascii_string& src, int encoding) {
+        switch(encoding) {
+        case Ascii:
+        case ShiftJis:
+            return babel::sjis_to_unicode(src);
+        case EucJp:
+            return babel::euc_to_unicode(src);
+        case Jis:
+            return babel::jis_to_unicode(src);
+        case Utf8:
+        {
+            auto copy = src;
+            Mac::Normalize(copy);
+            return babel::utf8_to_unicode(copy);
         }
+        case Unicode:
+        default:
+            break;
+        }
+        return Widen(src);
+    }
 
-        /* ----------------------------------------------------------------- */
-        ///
-        /// ToUnicode
-        ///
-        /// <summary>
-        /// Unicode に変換します。
-        /// </summary>
-        ///
-        /* ----------------------------------------------------------------- */
-        static unicode_string ToUnicode(const ascii_string& src) {
-            auto encoding = Guess(src);
-            return ToUnicode(src, encoding);
-        }
+    /* --------------------------------------------------------------------- */
+    ///
+    /// ToUnicode
+    ///
+    /// <summary>
+    /// Unicode に変換します。
+    /// </summary>
+    ///
+    /* --------------------------------------------------------------------- */
+    static unicode_string ToUnicode(const ascii_string& src) {
+        return ToUnicode(src, Guess(src));
+    }
 
-        /* ----------------------------------------------------------------- */
-        ///
-        /// ToUtf8
-        ///
-        /// <summary>
-        /// UTF-8 に変換します。
-        /// </summary>
-        ///
-        /* ----------------------------------------------------------------- */
-        static ascii_string ToUtf8(const unicode_string& src) {
-            return babel::unicode_to_utf8(src);
-        }
+    /* --------------------------------------------------------------------- */
+    ///
+    /// ToUtf8
+    ///
+    /// <summary>
+    /// UTF-8 に変換します。
+    /// </summary>
+    ///
+    /* --------------------------------------------------------------------- */
+    static ascii_string ToUtf8(const unicode_string& src) {
+        return babel::unicode_to_utf8(src);
+    }
 
-        /* ----------------------------------------------------------------- */
-        ///
-        /// ToShiftJis
-        ///
-        /// <summary>
-        /// Shift_JIS に変換します。
-        /// </summary>
-        ///
-        /* ----------------------------------------------------------------- */
-        static ascii_string ToShiftJis(const unicode_string& src) {
-            return babel::unicode_to_sjis(src);
-        }
+    /* --------------------------------------------------------------------- */
+    ///
+    /// ToShiftJis
+    ///
+    /// <summary>
+    /// Shift_JIS に変換します。
+    /// </summary>
+    ///
+    /* --------------------------------------------------------------------- */
+    static ascii_string ToShiftJis(const unicode_string& src) {
+        return babel::unicode_to_sjis(src);
+    }
+    
+    /* --------------------------------------------------------------------- */
+    ///
+    /// Widen
+    ///
+    /// <summary>
+    /// 標準 API を用いて string を wstring に変換します。
+    /// </summary>
+    ///
+    /* --------------------------------------------------------------------- */
+    static unicode_string Widen(const ascii_string& src, UINT cp = CP_OEMCP) {
+        wchar_t buffer[2048] = {};
+        auto count  = static_cast<int>(sizeof(buffer) / sizeof(buffer[0]));
+        auto result = ::MultiByteToWideChar(cp, 0, src.c_str(), -1, buffer, count);
+        return (result != 0) ? unicode_string(buffer) : unicode_string();
+    }
 
-    private:
-        /* ----------------------------------------------------------------- */
-        ///
-        /// Widen
-        ///
-        /// <summary>
-        /// string を wstring に変換します。
-        /// </summary>
-        ///
-        /* ----------------------------------------------------------------- */
-        static unicode_string Widen(const ascii_string& src) {
-            wchar_t buffer[2048] = {};
-            auto result = ::MultiByteToWideChar(CP_ACP, 0, src.c_str(), -1, buffer, sizeof(buffer) / sizeof(wchar_t));
-            return (result != 0) ? unicode_string(buffer) : unicode_string();
-        }
-    }; // Conversion
+    /* --------------------------------------------------------------------- */
+    ///
+    /// Narrow
+    ///
+    /// <summary>
+    /// 標準 API を用いて wstring を string に変換します。
+    /// </summary>
+    ///
+    /* --------------------------------------------------------------------- */
+    static ascii_string Narrow(const unicode_string& src, UINT cp = CP_OEMCP) {
+        char buffer[2048] = {};
+        auto count  = static_cast<int>(sizeof(buffer) / sizeof(buffer[0]));
+        auto result = WideCharToMultiByte(cp, 0, src.c_str(), -1, buffer, count, nullptr, nullptr);
+        return result != 0 ? ascii_string(buffer) : ascii_string();
+    }
+}; // Conversion
+
 }} // Cube::Encoding
-
-#endif // CUBE_ENCODING_CONVERSION_H
